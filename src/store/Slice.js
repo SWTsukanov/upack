3d666b86ae1cdf7ad3e0d494a152449ee96caff4
VIKolko <kolko.valentin@gmail.com>
2022-02-18 13:18:13 +0300

    axios slice.js

--- src/store/Slice.js
+++ src/store/Slice.js
@@ -1,9 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-
+    loading:false,
+    dataProducts:[],
+    error:''
 };
 
-export const catalogSlice = ({
+export const catalogSlice = createSlice({
     
 })
