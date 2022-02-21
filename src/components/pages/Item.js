cbce168683d106175b2faebbc1e2b9d70ea8399d
VIKolko <kolko.valentin@gmail.com>
2022-02-21 17:53:21 +0300

    blank for item components

--- src/components/pages/Item.js
+++ src/components/pages/Item.js
@@ -1,11 +1,34 @@
 import React from "react";
+import styled from "styled-components";
+import MPath from "../molecule/M_Path";
+import OCardItem from "../organism/O_CardItem";
+import OMenuFooter from "../organism/O_MenuFooter";
+import OMenuHeader from "../organism/O_MenuHeader";
 
 const Item = () => {
 
     return(
-        <>
-            PAGE ITEM
-        </>
+        <div>
+           <OMenuHeader />
+           <Hr />
+           <OCardItem />
+
+           <MPath />
+
+           <OMenuFooter />
+        </div>
     )
 }
 export default Item;
+
+const Div = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+`
+
+const Hr = styled.hr`
+margin-top:46px;
+width:100%;
+`
